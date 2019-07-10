66290fbd0bd6229fdf8648a977d80fbbcc7915ef
Mantab Tadayon <mt.tadayon@gmail.com>
2019-07-10 12:07:07 +1200

    Make correction for typo "websitre" to "website"

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'websitre-clone'`, () => {
+  it(`should have as title 'website-clone'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('websitre-clone');
+    expect(app.title).toEqual('website-clone');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to websitre-clone!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to website-clone!');
   });
 });
