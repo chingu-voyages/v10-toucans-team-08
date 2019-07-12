b3b4c1bb21fcb3093b03437d0440258a4bff6aa1
Michiel Slebos <michiel.slebos@gmail.com>
2019-07-12 16:01:47 +0900

    build section component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule,
