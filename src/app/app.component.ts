753b232551c212227a952fe9bd700c6dc77795d3
Mantab Tadayon <mt.tadayon@gmail.com>
2019-07-16 10:29:14 +1200

    Add button with fontawesome icon and properties: color, position and visibility.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Colors, PositionOfButton} from './scroll-section-btn/scroll-section-btn.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  buttonColor: Colors = Colors.WHITE;
+  positionButton: PositionOfButton = PositionOfButton.BOTTOM;
+  buttonVisibleOnSection = true;
   title = 'website-clone';
+
+  /*This click event (colorOfButton) should be embedded in the app.component.html - sections,
+   so that we can change the status of button depend on the section number*/
+  /*public colorOfButton(currentPos: number) {
+    this.buttonColor = currentPos === 2 || currentPos === 6 || currentPos === 7 ? Colors.BLACK : Colors.WHITE;
+    this.buttonVisibleOnSection = !(currentPos === 3 || currentPos === 8);
+  }*/
+
+  constructor() {
+  }
+
 }
--- src/app/scroll-section-btn/scroll-section-btn.component.ts
+++ src/app/scroll-section-btn/scroll-section-btn.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { faArrowAltCircleDown } from '@fortawesome/free-regular-svg-icons';
+
+export enum Colors {
+  WHITE = 'white',
+  BLACK = 'black',
+}
+
+export enum PositionOfButton {
+  TOP = 'top',
+  BOTTOM = 'bottom'
+  }
 
 @Component({
   selector: 'app-scroll-section-btn',
@@ -6,8 +17,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scroll-section-btn.component.css']
 })
 export class ScrollSectionBtnComponent implements OnInit {
+  @Input() public color: Colors;
+  @Input() public positionButton: PositionOfButton;
+  @Input() public visible: boolean;
+  iconButton = faArrowAltCircleDown;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
