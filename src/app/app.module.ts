ffa86b0255e679e8de8fb53b16f7213ac56ed1b9
Kohki Shiga <khkshg@gmail.com>
2019-07-16 06:12:08 +0900

    Add FontAwesome Icons for Main-menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { MainMenuComponent } from './main-menu/main-menu.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {faTimes} from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.css']
 })
 export class MainMenuComponent implements OnInit {
+  faTimes = faTimes;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
