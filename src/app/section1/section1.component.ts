553b92150bbc3a83bb8c502e8a8aebaa701b9576
Michiel Slebos <michiel.slebos@gmail.com>
2019-07-21 12:21:41 +0900

    remove icons & get rid of sanitizer

--- src/app/section1/section1.component.ts
+++ src/app/section1/section1.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, animate, transition, keyframes } from '@angular/animations';
-import { DomSanitizer } from '@angular/platform-browser';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 
 @Component({
   selector: 'app-section1',
