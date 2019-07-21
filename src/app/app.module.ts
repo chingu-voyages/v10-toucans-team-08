ed37e0aaed0e3aef1da5ab2a3859d7e05b2ce154
Mantab Tadayon <mt.tadayon@gmail.com>
2019-07-21 16:32:35 +1200

    Change the order of component and fix it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { Section1Component } from './section1/section1.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { Icon1Component } from './icon1/icon1.component';
-import { Icon2Component } from './icon2/icon2.component';
-import { Icon3Component } from './icon3/icon3.component';
-import { Icon4Component } from './icon4/icon4.component';
-import { IconComponent } from './icon/icon.component';
-import { ScrollSectionBtnComponent } from './scroll-section-btn/scroll-section-btn.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {Section1Component} from './section1/section1.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {Icon1Component} from './icon1/icon1.component';
+import {Icon2Component} from './icon2/icon2.component';
+import {Icon3Component} from './icon3/icon3.component';
+import {Icon4Component} from './icon4/icon4.component';
+import {IconComponent} from './icon/icon.component';
+import {ScrollSectionBtnComponent} from './scroll-section-btn/scroll-section-btn.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import { MainMenuComponent } from './main-menu/main-menu.component';
+import {MainMenuComponent} from './main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
@@ -30,11 +30,11 @@ import { MainMenuComponent } from './main-menu/main-menu.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
-    AppRoutingModule,
+    BrowserAnimationsModule,
     FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
