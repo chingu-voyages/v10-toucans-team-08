5ec4a8e8007971ac711f4474b4b46f03cd81746e
Mantab Tadayon <mt.tadayon@gmail.com>
2019-08-05 09:17:41 +1200

    Add content of all 3 subsection and add style to it.

--- src/app/section3/section3-subsection/section3-subsection.component.ts
+++ src/app/section3/section3-subsection/section3-subsection.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {faMountain} from '@fortawesome/free-solid-svg-icons/faMountain';
+import {faFlag} from '@fortawesome/free-regular-svg-icons';
+import {faCompass} from '@fortawesome/free-regular-svg-icons/faCompass';
+
 
 @Component({
   selector: 'app-section3-subsection',
   templateUrl: './section3-subsection.component.html',
   styleUrls: ['./section3-subsection.component.css']
 })
-export class Section3SubsectionComponent implements OnInit {
+export class Section3SubsectionComponent {
+  @Input() sectionPosition = 0;
 
-  constructor() { }
+  public iconMountain = faMountain;
+  public iconCompass = faCompass;
+  public iconFlag = faFlag;
 
-  ngOnInit() {
-  }
 
 }
