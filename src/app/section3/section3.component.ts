2c7f23125fe52254c97194218934d8a14b959de9
Mantab Tadayon <mt.tadayon@gmail.com>
2019-08-08 13:25:49 +1200

    Move all code from subsection3 to section3 and show and hide subsection on click event

--- src/app/section3/section3.component.ts
+++ src/app/section3/section3.component.ts
@@ -1,4 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
+import {faMountain} from '@fortawesome/free-solid-svg-icons/faMountain';
+import {faCompass} from '@fortawesome/free-regular-svg-icons/faCompass';
+import {faFlag} from '@fortawesome/free-regular-svg-icons';
+import {faArrowDown} from '@fortawesome/free-solid-svg-icons/faArrowDown';
 
 @Component({
   selector: 'app-section3',
@@ -6,5 +10,34 @@ import {Component} from '@angular/core';
   styleUrls: ['./section3.component.css']
 })
 export class Section3Component {
+  public iconMountain = faMountain;
+  public iconCompass = faCompass;
+  public iconFlag = faFlag;
+  public iconArrowDown = faArrowDown;
+  public isChallengeSubsectionHidden = false;
+  public isSolutionSubsectionHidden = true;
+  public isImpactSubsectionHidden = true;
 
+  public sectionVisible(sectionName: string) {
+    switch (sectionName) {
+      case 'challenge': {
+        this.isChallengeSubsectionHidden = !this.isChallengeSubsectionHidden;
+        this.isSolutionSubsectionHidden = true;
+        this.isImpactSubsectionHidden = true;
+        break;
+      }
+      case 'solution': {
+        this.isSolutionSubsectionHidden = !this.isSolutionSubsectionHidden;
+        this.isChallengeSubsectionHidden = true;
+        this.isImpactSubsectionHidden = true;
+        break;
+      }
+      case 'impact': {
+        this.isImpactSubsectionHidden = !this.isImpactSubsectionHidden;
+        this.isChallengeSubsectionHidden = true;
+        this.isSolutionSubsectionHidden = true;
+        break;
+      }
+    }
+  }
 }
