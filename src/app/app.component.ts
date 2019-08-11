67196e0c205661ecdc566cab7a93ea36763b41f4
Michiel Slebos <michiel.slebos@gmail.com>
2019-08-11 18:09:25 +0200

    refactor code and finish section4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {Colors, PositionOfButton} from './scroll-section-btn/scroll-section-btn.component';
+import { Section4Component } from './section4/section4.component';
 
 enum SectionId {
   SECTIONONE = 'sectionOne',
@@ -18,14 +19,18 @@ enum SectionId {
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild(Section4Component, {static: false}) child;
   public sectionId = SectionId;
   public positionOfView: SectionId = SectionId.SECTIONONE;
   buttonColor: Colors = Colors.WHITE;
   positionButton: PositionOfButton = PositionOfButton.BOTTOM;
   buttonVisibleOnSection = true;
+  ngOnInit() {
+  }
 
   public statusOfButton(sectionId) {
+    // tslint:disable-next-line: max-line-length
     this.buttonColor = sectionId === 'sectionTwo' || sectionId === 'sectionSix' || sectionId === 'sectionSeven' ? Colors.BLACK : Colors.WHITE;
     this.buttonVisibleOnSection = !(sectionId === 'sectionEight');
   }
@@ -38,6 +43,11 @@ export class AppComponent {
       }
       case SectionId.SECTIONTWO: {
         this.positionOfView = this.sectionId.SECTIONTHREE;
+        break;
+      }
+      case SectionId.SECTIONTHREE: {
+        this.positionOfView = this.sectionId.SECTIONFOUR;
+        this.child.initializeLoadingProgress();
         break;
       }
     }
--- src/app/section4/section4.component.ts
+++ src/app/section4/section4.component.ts
@@ -1,47 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-section4',
   templateUrl: './section4.component.html',
   styleUrls: ['./section4.component.css']
 })
+
+// enum ImageUrl {
+//   IMAGEONE = 'url(/assets/background-section4-1.jpg)',
+//   IMAGETWO = 'url(/assets/background-section4-2.jpg)',
+//   IMAGETHREE = 'url(/assets/background-section4-3.jpg)'
+// }
+
 export class Section4Component implements OnInit {
-  p1 = '…identified new drug candidates in the fight against neuroblastoma – a childhood cancer.';
-  p2 = '…discovered how nanotechnology could help provide clean water to millions.';
-  p3 = '…discovered new compounds for harnessing solar power.';
+  p1 = '…identified new drug candidates in the fight against neuroblastoma – a childhood&nbsp;cancer.';
+  p2 = '…discovered how nanotechnology could help provide clean water to&nbsp;millions.';
+  p3 = '…discovered new compounds for harnessing solar&nbsp;power.';
   text = this.p1;
   widthInt = 0.1;
   color = '#D7AAFF';
   buttonHidden = true;
-
+  backgroundUrl1 = 'url(/assets/background-section4-1.jpg)';
+  backgroundUrl2 = 'url(/assets/background-section4-2.jpg)';
+  backgroundUrl3 = 'url(/assets/background-section4-3.jpg)';
+  backgroundUrl = this.backgroundUrl1;
+  // @Input() imageUrl = ImageUrl;
 
 
   constructor() { }
 
   ngOnInit() {
-    setInterval(() => {
-        this.widthInt += .1;
-        if (this.widthInt > 33) {
-          this.color = '#5596E6';
-          this.text = this.p2;
-        }
-        if (this.widthInt > 66) {
-          this.color = '#FDD500';
-          this.text = this.p3;
-        }
-        if (this.widthInt > 99) {
-          this.buttonHidden = false;
-        }
-    }, 10);
+    // this.initializeLoadingProgress();
   }
 
-  startAgain = function() {
+   initializeLoadingProgress() {
+    setInterval(() => {
+      this.widthInt += .1;
+      if (this.widthInt > 33) {
+        this.color = '#5596E6';
+        this.text = this.p2;
+        this.backgroundUrl = this.backgroundUrl2;
+      }
+      if (this.widthInt > 66) {
+        this.color = '#FDD500';
+        this.text = this.p3;
+        this.backgroundUrl = this.backgroundUrl3;
+      }
+      if (this.widthInt > 99) {
+        this.buttonHidden = false;
+      }
+    }, 10);
+  }
+  startAgain = () => {
     this.widthInt = 0.1;
     this.buttonHidden = true;
     this.color = '#D7AAFF';
     this.text = this.p1;
-  };
-
-
-
+    this.backgroundUrl = this.backgroundUrl1;
+  }
 }
