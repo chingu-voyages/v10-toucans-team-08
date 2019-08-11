5a6abb105c66e9c539f83349837c4f8c64bbe5b2
Mantab Tadayon <mt.tadayon@gmail.com>
2019-08-12 09:13:42 +1200

    Tidy up unused code

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 import {faTimes} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -6,12 +6,6 @@ import {faTimes} from '@fortawesome/free-solid-svg-icons';
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.css']
 })
-export class MainMenuComponent implements OnInit {
+export class MainMenuComponent {
   faTimes = faTimes;
-
-  constructor() {
-  }
-
-  ngOnInit() {
-  }
 }
