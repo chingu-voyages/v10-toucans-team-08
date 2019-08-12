4481aec2bb7272ee5c17aa2c6cffaa6cba03fdb1
Mantab Tadayon <mt.tadayon@gmail.com>
2019-08-12 13:51:54 +1200

    Add text and style to section 2

--- src/app/section2/section2.component.ts
+++ src/app/section2/section2.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-section2',
   templateUrl: './section2.component.html',
   styleUrls: ['./section2.component.css']
 })
-export class Section2Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class Section2Component {
 
 }
