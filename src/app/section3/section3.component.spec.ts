f16deba320bd57f5f2e705d57e0eab2f17855bf0
Mantab Tadayon <mt.tadayon@gmail.com>
2019-08-13 20:34:03 +1200

    Fix test for section3 component

--- src/app/section3/section3.component.spec.ts
+++ src/app/section3/section3.component.spec.ts
@@ -1,6 +1,11 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { Section3Component } from './section3.component';
+import {Section3Component} from './section3.component';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {faMountain} from '@fortawesome/free-solid-svg-icons/faMountain';
+import {faCompass} from '@fortawesome/free-regular-svg-icons/faCompass';
+import {faFlag} from '@fortawesome/free-regular-svg-icons';
+import {faArrowDown} from '@fortawesome/free-solid-svg-icons/faArrowDown';
 
 describe('Section3Component', () => {
   let component: Section3Component;
@@ -8,9 +13,10 @@ describe('Section3Component', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ Section3Component ]
+      declarations: [Section3Component],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +25,19 @@ describe('Section3Component', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create app-section3', () => {
     expect(component).toBeTruthy();
+  });
+  it('should contain mountain icon', () => {
+    expect(component.iconMountain).toBe(faMountain);
+  });
+  it('should contain compass icon', () => {
+    expect(component.iconCompass).toBe(faCompass);
+  });
+  it('should contain flag icon', () => {
+    expect(component.iconFlag).toBe(faFlag);
+  });
+  it('should contain arrow-down icon', () => {
+    expect(component.iconArrowDown).toBe(faArrowDown);
   });
 });
