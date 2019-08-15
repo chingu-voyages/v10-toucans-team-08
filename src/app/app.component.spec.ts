cc57d9122a6bcf3750402c8acd7aa91c9ad69b59
Michiel Slebos <47251816+MichielSlebos@users.noreply.github.com>
2019-08-15 10:55:43 +0200

    Merge development into master (#64)
    * First Commit --- Boilerplate for Angular Framework
    
    * Add .gitignore to prevent push idea's files.
    
    * Delete all not necessary files
    
    * Add angular boilerplate to the project
    
    * Make correction for typo "websitre" to "website"
    
    * modify README.md to include Chingu-info (#12)
    
    * Add component for scroll-button
    
    * Add fontawesome for regular icons to project
    
    * build section component
    
    * Add fontawesome to app.module
    
    * Create component for main-menu
    
    Used the following command to create main-menu component.
    
    ng generate component main-menu
    
    * Show logos on the left side and Login/Join on the right side
    
    * Add FontAwesome Icons for Main-menu
    
    * implemented media queries for the left side logos
    
    * Add button with fontawesome icon and properties: color, position and visibility.
    
    * make section modular
    
    * Mahtab/fix readme url (#22)
    
    Change URL for our cloning website
    
    * Create right side of the menu
    
    * Create test for the component and for fontawesome icon
    
    * Tidy up and remove title and constructor, change method's name as it's for the status of button and not just color
    
    * Generalise the variables for future tests and add test case for initialise color of button
    
    * Created design for the entire menu. I also deleted comments and source code that are not used.
    
    * Applied url for all links
    
    * Delete unnecessary code for
    
    * Add box shadow at the bottom of the header
    
    * javascript animations
    
    * javascript section1
    
    * delete safe pipe
    
    * Follow the BEM CSS style
    
    * Swap image using media queries
    
    * fix svg code laptop
    
    * remove icons & get rid of sanitizer
    
    * Tidy up the imports files
    
    * Change the order of component and fix it (#25)
    
    * Mahtab/heroku deployment (#26)
    
    * Change the order of component and fix it
    
    * Add changes to fix deployment on heruko
    
    * Fix the position of button, change button's size adjust the media query (#34)
    
    * Add scroll to click event for button, add scroll smooth as behavior to css
    
    * create HTML+CSS navigation-carousel
    
    * Take out the scroll responsibility from child-component(scroll-section-btn.component) and put it in app.component
    
    * Koki.shiga/heroku deployment (#38)
    
    Fix the deployment code for Heroku
    
    * Fixed main menu (#37)
    
    Fix main menu position
    
    * remove hover effect
    
    * Uncomment the main-menu and tidy up the styles.css
    
    * fix js icons (#35)
    
    Fix JavaScript for icons' animation and fix variables' name
    
    * fix functionality nav-carousel (#42)
    
    Add design and functionality to the nav-carousel.
    
    * Fixed main menu's position at the top. (#43)
    
    * Fixed main menu's position at the top.
    
    Also fixed the font size and hover effect on the right side of the menu. (Login Join button)
    
    * Add z-index: 1000
    
    * Scroll down and up functionality (#44)
    
    * Tidy up the scrollDown-method and change the scrollIntoView to scrolling with css. Add scrollDown-method to scroll to exact position.
    
    * Fix the color change for button on base of position of view
    
    * Add transition to button to change the colour slowly
    
    * Add transition effect for slow start and end and adjust transition time
    
    * Add debounce to scroll functionality to put delay for scrolling
    
    * Complete the scroll functionality for scroll down and up and adjust css
    
    * Fix style for navigation and height for wrapper.
    
    * Test for app-component, mein-menu-component, section1
    
    * Create section 3  and all 3 Subsection with scroll down and up functionality
    
    * Create section4
    
    * Create section two
    
    * Tidy up and fix some bugs
    
    * Improve section 3, write tests, fix bugs
    
    * fixed navigation-carousel css
    
    * Fix navigation button to show on ImpactSubsection and hide on SolutionSubsection,
    
    * Mahtab/fix navigation carousel (#63)
    
    * Add scrolling functionality to navigation bar and fix the style for navigation list
    
    * Add loading progress to section four of navigation bar

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {AppComponent, SectionId} from './app.component';
 import {Colors, ScrollSectionBtnComponent} from './scroll-section-btn/scroll-section-btn.component';
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {SubsectionId} from './section3/section3.component';
 
 describe('AppComponent', () => {
   let app: AppComponent;
@@ -30,15 +31,113 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  describe('The button status for color', () => {
+  describe('The button status for color and visibility', () => {
     it('should be initialised with white color', () => {
       expect(app.buttonColor).toBe(Colors.WHITE);
     });
+    it('should be visible on SubsectionId.IMPACT of sectionThree', () => {
+      app.positionOfSubsectionThree = SubsectionId.IMPACT;
+      expect(app.buttonVisibleOnSection).toBe(true);
+    });
   });
 
-  describe('The position of view for app', () => {
+  describe('The position of view for app with onClickToScrollDown', () => {
     it('Position of view should be initialised with section one', () => {
       expect(app.positionOfView).toBe(SectionId.SECTIONONE);
+    });
+
+    it('should change from SectionOne to SectionTwo with scrollDown', () => {
+      app.positionOfView = SectionId.SECTIONONE;
+      app.onClickToScrollDown();
+      expect(app.positionOfView).toBe(SectionId.SECTIONTWO);
+    });
+
+    it('should change from sectionTwo to SectionThree with scrollDown', () => {
+      app.positionOfView = SectionId.SECTIONTWO;
+      app.onClickToScrollDown();
+      expect(app.positionOfView).toBe(SectionId.SECTIONTHREE);
+    });
+
+    it('should change from sectionThree to sectionFour with scrollDown', () => {
+      app.positionOfView = SectionId.SECTIONTHREE;
+      app.onClickToScrollDown();
+      expect(app.positionOfView).toBe(SectionId.SECTIONFOUR);
+    });
+
+    it('should stay on sectionFour with scrollDown', () => {
+      app.positionOfView = SectionId.SECTIONFOUR;
+      app.onClickToScrollDown();
+      expect(app.positionOfView).toBe(SectionId.SECTIONFOUR);
+    });
+  });
+
+  describe('The position of view for app with scrollView', () => {
+    it('should increase by one hundred if scrollDown is true', () => {
+      app.scrollValue = 0;
+      app.scrollView(true);
+      expect(app.scrollValue).toBe(100);
+    });
+
+    it(' should decrease by one hundred if scrollDown is false', () => {
+      app.scrollValue = 0;
+      app.scrollView(false);
+      expect(app.scrollValue).toBe(0);
+    });
+
+    it(' should not go over 300', () => {
+      app.scrollValue = 300;
+      app.scrollView(true);
+      expect(app.scrollValue).toBe(300);
+    });
+  });
+
+  describe('sectionThreeSubSectionScrolling:', () => {
+    it('should change positionOfSubsectionThree from CHALLENGE to SOLUTION', () => {
+      app.positionOfSubsectionThree = SubsectionId.CHALLENGE;
+      app.sectionThreeSubSectionScrolling(true, SubsectionId.CHALLENGE);
+      expect(app.positionOfSubsectionThree).toBe(SubsectionId.SOLUTION);
+    });
+
+    it('should change positionOfSubsectionThree from SOLUTION to IMPACT', () => {
+      app.positionOfSubsectionThree = SubsectionId.SOLUTION;
+      app.sectionThreeSubSectionScrolling(true, SubsectionId.SOLUTION);
+      expect(app.positionOfSubsectionThree).toBe(SubsectionId.IMPACT);
+    });
+
+    it('should change positionOfSubsectionThree from IMPACT to SOLUTION', () => {
+      app.positionOfSubsectionThree = SubsectionId.IMPACT;
+      app.sectionThreeSubSectionScrolling(false, SubsectionId.IMPACT);
+      expect(app.positionOfSubsectionThree).toBe(SubsectionId.SOLUTION);
+    });
+
+    it('should change positionOfSubsectionThree from SOLUTION to CHALLENGE', () => {
+      app.positionOfSubsectionThree = SubsectionId.SOLUTION;
+      app.sectionThreeSubSectionScrolling(false, SubsectionId.SOLUTION);
+      expect(app.positionOfSubsectionThree).toBe(SubsectionId.CHALLENGE);
+    });
+  });
+
+  describe('leaveSectionThree', () => {
+    it('should change from subsection IMPACT to section Four with scrollDown', () => {
+      app.positionOfView = SectionId.SECTIONTHREE;
+      app.positionOfSubsectionThree = SubsectionId.IMPACT;
+      app.leaveSectionThree(true);
+      expect(app.positionOfView).toBe(SectionId.SECTIONFOUR);
+    });
+
+    it('should change from subsection CHALLENGE to section Two with scrollDown', () => {
+      app.positionOfView = SectionId.SECTIONTHREE;
+      app.positionOfSubsectionThree = SubsectionId.CHALLENGE;
+      app.leaveSectionThree(false);
+      expect(app.positionOfView).toBe(SectionId.SECTIONTWO);
+    });
+  });
+
+  describe('scrollDown', () => {
+    it('should change from sectionTwo to sectionThree and stay on Subsection CHALLENGE', () => {
+      app.positionOfView = SectionId.SECTIONTWO;
+      app.scrollDown();
+      expect(app.positionOfSubsectionThree).toBe(SubsectionId.CHALLENGE);
     });
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, HostListener, ViewChild} from '@angular/core';
 import {Colors, PositionOfButton} from './scroll-section-btn/scroll-section-btn.component';
-import { Section4Component } from './section4/section4.component';
+import {Section4Component} from './section4/section4.component';
 import {debounce} from './decorators';
 import {SubsectionId} from './section3/section3.component';
 
@@ -8,11 +8,7 @@ export enum SectionId {
   SECTIONONE = 'sectionOne',
   SECTIONTWO = 'sectionTwo',
   SECTIONTHREE = 'sectionThree',
-  SECTIONFOUR = 'sectionFour',
-  SECTIONFIVE = 'sectionFive',
-  SECTIONSIX = 'sectionSix',
-  SECTIONSEVEN = 'sectionSeventh',
-  SECTIONEIGHT = 'sectionEight'
+  SECTIONFOUR = 'sectionFour'
 }
 
 @Component({
@@ -25,30 +21,32 @@ export class AppComponent {
   @ViewChild(Section4Component, {static: false}) child;
   public sectionId = SectionId;
   public positionOfView: SectionId = SectionId.SECTIONONE;
-  buttonColor: Colors = Colors.WHITE;
-  positionButton: PositionOfButton = PositionOfButton.BOTTOM;
-  buttonVisibleOnSection = true;
-  private lastScrollPosition = 0;
-  scrollValue = 0;
+  public buttonColor: Colors = Colors.WHITE;
+  public positionButton: PositionOfButton = PositionOfButton.BOTTOM;
+  public buttonVisibleOnSection = true;
+  public scrollValue = 0;
   public positionOfSubsectionThree: SubsectionId = SubsectionId.CHALLENGE;
   public subSectionEnum = SubsectionId;
+
+  private lastScrollPosition = 0;
 
   public statusOfButton(sectionId) {
     this.buttonColor = sectionId === 'sectionTwo'
     || sectionId === 'sectionSix'
     || sectionId === 'sectionSeven'
       ? Colors.BLACK : Colors.WHITE;
+
     this.buttonVisibleOnSection = !(sectionId === 'sectionThree' || sectionId === 'sectionEight');
   }
 
   public changePositionOfView(currentPosition: SectionId, goesDown: boolean) {
     switch (currentPosition) {
       case SectionId.SECTIONONE: {
-        this.positionOfView = goesDown ? this.sectionId.SECTIONTWO : SectionId.SECTIONONE;
+        this.positionOfView = goesDown ? this.sectionId.SECTIONTWO : this.sectionId.SECTIONONE;
         break;
       }
       case SectionId.SECTIONTWO: {
-        this.positionOfView = goesDown ? this.sectionId.SECTIONTHREE : SectionId.SECTIONONE;
+        this.positionOfView = goesDown ? this.sectionId.SECTIONTHREE : this.sectionId.SECTIONONE;
         break;
       }
       case SectionId.SECTIONTHREE: {
@@ -62,6 +60,7 @@ export class AppComponent {
       }
       case SectionId.SECTIONFOUR: {
         this.positionOfView = goesDown ? this.sectionId.SECTIONFOUR : SectionId.SECTIONTHREE;
+        this.positionOfView = goesDown ? this.sectionId.SECTIONFOUR : this.sectionId.SECTIONTHREE;
         break;
       }
       default:
@@ -70,55 +69,103 @@ export class AppComponent {
     this.statusOfButton(this.positionOfView);
   }
 
-  public onClickToScrollDown() {
-    this.scrollValue += 100;
-    this.changePositionOfView(this.positionOfView, true);
+  public onclickToNavigate(selectedSection: SectionId) {
+    if (selectedSection === SectionId.SECTIONFOUR) {
+      this.child.initializeLoadingProgress();
+    }
+    document.getElementById(selectedSection).scrollIntoView({block: 'center', behavior: 'smooth'});
+    this.positionOfView = selectedSection;
+    this.statusOfButton(selectedSection);
   }
 
-  @debounce(500)
+  @debounce(1000)
   @HostListener('window:scroll')
   public scrollDown() {
-    const currentScrollPosition = window.scrollY;
-    const scrollToDown = (currentScrollPosition > this.lastScrollPosition);
-    const scrollDelta = (currentScrollPosition - this.lastScrollPosition > 10)
-      || (currentScrollPosition - this.lastScrollPosition < 10);
+    if (window.scrollY === 10) {
+      return;
+    }
+    const scrollDOWN: boolean = this.getScrollDirection();
+
     if (this.positionOfView === this.sectionId.SECTIONTHREE) {
-      if (this.positionOfSubsectionThree === this.subSectionEnum.CHALLENGE && scrollToDown) {
-        this.positionOfSubsectionThree = this.subSectionEnum.SOLUTION;
-      } else if (this.positionOfSubsectionThree === this.subSectionEnum.CHALLENGE && !scrollToDown) {
-        this.scrollValue -= 100;
-        this.positionOfView = this.sectionId.SECTIONTWO;
-      } else if (this.positionOfSubsectionThree === SubsectionId.SOLUTION && scrollToDown) {
-        this.positionOfSubsectionThree = this.subSectionEnum.IMPACT;
-        this.buttonVisibleOnSection = true;
-      } else if (this.positionOfSubsectionThree === SubsectionId.SOLUTION && !scrollToDown) {
-        this.positionOfSubsectionThree = this.subSectionEnum.CHALLENGE;
-      } else if (this.positionOfSubsectionThree === this.subSectionEnum.IMPACT && scrollToDown) {
-        this.scrollValue += 100;
-        this.positionOfView = this.sectionId.SECTIONFOUR;
-      } else if (this.positionOfSubsectionThree === this.subSectionEnum.IMPACT && !scrollToDown) {
-        this.positionOfSubsectionThree = this.subSectionEnum.SOLUTION;
-      }
-    } else if (this.positionOfView === this.sectionId.SECTIONFOUR && scrollToDown) {
+      this.leaveSectionThree(scrollDOWN);
+      this.sectionThreeSubSectionScrolling(scrollDOWN, this.positionOfSubsectionThree);
+    } else if (this.positionOfView === this.sectionId.SECTIONFOUR && !scrollDOWN) {
+      this.changePositionOfView(this.positionOfView, scrollDOWN);
+      this.buttonVisibleOnSection = true;
+      this.scrollView(scrollDOWN);
+    } else {
+      this.scrollView(scrollDOWN);
+      this.changePositionOfView(this.positionOfView, scrollDOWN);
+    }
+    this.resetScroll();
+  }
+
+  public onClickToScrollDown() {
+    if (this.positionOfView === this.sectionId.SECTIONFOUR) {
       return;
     } else {
-      if (scrollToDown && scrollDelta) {
-        this.scrollValue += 100;
-        this.changePositionOfView(this.positionOfView, scrollToDown);
-      } else {
-        this.scrollValue -= 100;
-        this.changePositionOfView(this.positionOfView, scrollToDown);
+      this.scrollValue += 100;
+      this.changePositionOfView(this.positionOfView, true);
+    }
+  }
+
+  public leaveSectionThree(scrollDOWN: boolean) {
+    if (this.positionOfSubsectionThree === this.subSectionEnum.IMPACT && scrollDOWN) {
+      this.scrollView(scrollDOWN);
+      this.changePositionOfView(this.positionOfView, scrollDOWN);
+    } else if (this.positionOfSubsectionThree === this.subSectionEnum.CHALLENGE && !scrollDOWN) {
+      this.scrollView(scrollDOWN);
+      this.changePositionOfView(this.positionOfView, scrollDOWN);
+    }
+  }
+
+  private resetScroll() {
+    window.scrollTo(0, 10);
+    this.lastScrollPosition = 10;
+  }
+
+  public scrollView(scrollDOWN: boolean) {
+    if (!scrollDOWN && this.scrollValue === 0) {
+      return;
+    }
+
+    if (scrollDOWN && this.scrollValue === 300) {
+      return;
+    }
+
+    if (scrollDOWN) {
+      this.scrollValue += 100;
+    } else {
+      this.scrollValue -= 100;
+    }
+  }
+
+  private getScrollDirection(): boolean {
+    const boundingClientRect = window.document.body.getBoundingClientRect();
+    const scrollDown = boundingClientRect.top < -10;
+    this.lastScrollPosition = boundingClientRect.top;
+    return scrollDown;
+  }
+
+  public sectionThreeSubSectionScrolling(scrollDirectionDown, subSectionId: SubsectionId) {
+    if (scrollDirectionDown) {
+      if (subSectionId === this.subSectionEnum.CHALLENGE) {
+        this.positionOfSubsectionThree = this.subSectionEnum.SOLUTION;
+      } else if (subSectionId === this.subSectionEnum.SOLUTION) {
+        this.positionOfSubsectionThree = this.subSectionEnum.IMPACT;
+        this.buttonVisibleOnSection = true;
       }
-      this.lastScrollPosition = currentScrollPosition <= 0 ? 0 : currentScrollPosition;
+    } else {
+      if (subSectionId === this.subSectionEnum.IMPACT) {
+        this.positionOfSubsectionThree = this.subSectionEnum.SOLUTION;
+        this.buttonVisibleOnSection = false;
+      } else if (subSectionId === this.subSectionEnum.SOLUTION) {
+        this.positionOfSubsectionThree = this.subSectionEnum.CHALLENGE;
+      }
     }
   }
 
   onClickToSubsection() {
-    if (this.positionOfSubsectionThree === this.subSectionEnum.CHALLENGE) {
-      this.positionOfSubsectionThree = this.subSectionEnum.SOLUTION;
-    } else if (this.positionOfSubsectionThree === SubsectionId.SOLUTION) {
-      this.buttonVisibleOnSection = true;
-      this.positionOfSubsectionThree = this.subSectionEnum.IMPACT;
-    }
+    this.sectionThreeSubSectionScrolling(true, this.positionOfSubsectionThree);
   }
 }
